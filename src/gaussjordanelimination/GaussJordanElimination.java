55dfe5e1fbea142bd4d68ba01d165b44f92603a2
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2018-07-31 01:47:58 +0800

    problems

--- src/gaussjordanelimination/GaussJordanElimination.java
+++ src/gaussjordanelimination/GaussJordanElimination.java
@@ -5,6 +5,9 @@
  */
 package gaussjordanelimination;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Neil Michael Noble
@@ -17,10 +20,26 @@ public class GaussJordanElimination {
     public static void main(String[] args) {
         // TODO code application logic here
         Vector v = new Vector(0);
-        Vector v1 = new Vector(new double[]{1, 2, 3}, 3);
+        /*Vector v1 = new Vector(new double[]{1, 2, 3}, 3);
         Vector v2 = new Vector(new double[]{3, 2, 3}, 3);
         v1.add(v2);
+        v1.printer();*/
+        
+        System.out.println("\n");
+        
+        List<Vector> vectors = new ArrayList<Vector>();
+        Vector v1 = new Vector(new double[]{1, 1, 1}, 3);
+        Vector v2 = new Vector(new double[]{2, 3, 5}, 3);
+        Vector v3 = new Vector(new double[]{4, 0, 5}, 3);
+        Vector vR = new Vector(new double[]{5, 8, 2}, 3);
         v1.printer();
+        v2.printer();
+        v3.printer();
+        vR.printer();
+        vectors.add(v1);
+        vectors.add(v2);
+        vectors.add(v3);
+        v.Gauss_Jordan(vectors, 3, vR).printer();
     }
     
 }
--- src/gaussjordanelimination/Vector.java
+++ src/gaussjordanelimination/Vector.java
@@ -39,22 +39,74 @@ public class Vector {
     }
     
     void printer (){
+        System.out.println("\nPrinter: ");
         for(int i=0; i<dimension; i++)
-            System.out.println(array[i]);
+            System.out.print(array[i]+" ");
         
     }
     Vector Gauss_Jordan (List<Vector> vectors, int dimension, Vector constants){
+        System.out.println();
+        List<Vector> fVectors = vectors;
+                                    //System.out.println("G_J:");;
+                                    //System.out.println("G_J:");
         for(int i=0; i<vectors.size(); i++){
-            for (int j = i+1; j < vectors.size()-i; j++) {
-                if (vectors.get(j).array[j]!=0) {
-                    for(int k=0; k<dimension; k++){
-                        double[] temp = vectors.get(i).array;
+                                    //System.out.println("i=====: "+vectors.get(i).array[0]);
+                                    vectors.get(i).printer();
+            for(int j = i+1; j<vectors.size(); j++){
+                                    //System.out.println("j: "+j);
+                for(int k=i; k<dimension; k++){
+                    if(vectors.get(j).array[k]!=0){
+                                    //System.out.println("vec.("+i+").arr["+k+"]: "+vectors.get(i).array[k]
+                                    //        +" * vec.("+j+").arr["+i+"]: "+vectors.get(j).array[i]);
+                                    System.out.println("\ntemp  | ("+i+","+k+"): "+vectors.get(i).array[k]
+                                            +" * ("+j+","+i+"): "+vectors.get(j).array[i]);
+                                    System.out.println("temp2 | ("+j+","+k+"): "+vectors.get(j).array[k]
+                                            +" * ("+i+","+i+"): "+vectors.get(i).array[i]);
+                        double temp = vectors.get(i).array[k]*vectors.get(j).array[i];
+                        double temp2 = vectors.get(j).array[k]*vectors.get(i).array[i];
+                                    System.out.print(k+": "+temp);
+                                    System.out.println("_"+temp2);
+                        if(vectors.get(i).array[i]>0){
+                            if (vectors.get(j).array[i] > 0) 
+                                fVectors.get(j).array[k] = temp - temp2;
+                            if (vectors.get(j).array[i] < 0) 
+                                fVectors.get(j).array[k] = temp + temp2;
+                        }
+                            
+                        if(vectors.get(i).array[i]<0){
+                            if (vectors.get(j).array[i] > 0) 
+                                fVectors.get(j).array[k] = temp + temp2;
+                            if (vectors.get(j).array[i] < 0) 
+                                fVectors.get(j).array[k] = temp - temp2;
+                        }
+                        
+                        if(vectors.get(j).array[k]!=0){
+                            double conTemp = constants.array[i]*vectors.get(j).array[i];
+                            double conTemp2 = constants.array[j]*vectors.get(i).array[i];
+                            if(vectors.get(i).array[i]>0){
+                                if (vectors.get(j).array[i] > 0) 
+                                    constants.array[k] = conTemp - conTemp2;
+                                if (vectors.get(j).array[i] < 0) 
+                                    constants.array[k] = conTemp + conTemp2;
+                            }
+
+                            if(vectors.get(i).array[i]<0){
+                                if (vectors.get(j).array[i] > 0) 
+                                    constants.array[k] = temp + temp2;
+                                if (vectors.get(j).array[i] < 0) 
+                                    constants.array[k] = temp - temp2;
+                            }
+                            System.out.println("con");
+                        }
                     }
                 }
             }
         }
+        Vector finalV = new Vector(constants.array, dimension);
+        System.out.println("ret");
+        return finalV;
     }
-//    
+    
 //    int span (List<Vector> vectors, int dimension){
 //        
 //    }
